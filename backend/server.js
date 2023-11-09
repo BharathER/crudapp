0012561fed00d643e6cf6a5764870e86c5508cbf
Bharath3993 <bharath.e.radhakrishnan@gmail.com>
2023-11-09 16:57:54 +0530

    package json modification

--- backend/server.js
+++ backend/server.js
@@ -180,6 +180,12 @@ app.put("/api/data", (req, res) => {
 
 // Your API endpoints and routes go here
 
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+);
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
